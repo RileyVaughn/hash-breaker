e8d1fbe5f49268ae01de3029043fb1324a8655af
RileyVaughn <rj.vaughn@live.com>
2021-09-24 15:04:31 -0600

    incrembit fixed

--- main.go
+++ main.go
@@ -5,21 +5,32 @@ import "fmt"
 func main() {
 	b := []byte{0}
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(b)
-		b = incremBit(b)
+	for i := 0; i < 1000; i++ {
+
+		fmt.Printf("%08b", b)
+		println()
+		incremBit(&b)
 	}
 
 }
 
-func incremBit(b []byte) []byte {
+func incremBit(b *[]byte) {
 
-	for i := len(b) - 1; i >= 0; i-- {
-		if b[i] == byte(0) {
-			b[i] = byte(1)
-			return b
+	for i := len(*b) - 1; i >= 0; i-- {
+		if (*b)[i] != byte(255) {
+			(*b)[i]++
+			for j := i + 1; j < len(*b); j++ {
+				(*b)[j] = byte(0)
+			}
+			return
 		}
 	}
-	b = append([]byte{1}, b...)
-	return b
+
+	// Append new byte to front of slice. Only happens slice is if full of 255's
+	(*b)[0] = byte(1)
+	for i := 1; i < len(*b); i++ {
+		(*b)[i] = byte(0)
+	}
+	*b = append(*b, byte(0))
+	return
 }
